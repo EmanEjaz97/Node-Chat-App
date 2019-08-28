8bfb496d55c7e5cbeee7555983f2580e6e9cbc58
EmanEjaz97 <51271816+EmanEjaz97@users.noreply.github.com>
2019-08-28 17:56:07 +0500

    add alert on profanity

--- public/js/chat.js
+++ public/js/chat.js
@@ -48,7 +48,7 @@ $messageForm.addEventListener("submit", e => {
     $messageFormInput.focus();
 
     if (error) {
-      return console.log(error);
+      return alert(error);
     }
 
     console.log("Msg delivered");
